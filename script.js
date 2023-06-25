9d52eb0a8ba54a15582f15bfae8ca53f576e8373
Benz Samson Tagle <benztagle123@gmail.com>
2023-06-26 03:05:39 +0800

    Created Initial  JavaScript R-P-S Game Logic, currently works on console

---
+++ script.js
@@ -0,0 +1,71 @@
+function getComputerChoice() {
+    let randomValue = Math.floor(Math.random() * 3);
+    let handValue;
+    // console.log(randomValue);
+
+    switch(randomValue){
+        case 0: 
+            handValue = "Rock";
+            break;
+        
+        case 1:
+            handValue = "Paper";
+            break;
+        
+        case 2:
+            handValue = "Scissors";
+            break;
+    }
+
+    // console.log(handValue);
+    return handValue;
+}
+
+
+
+    let  computer = getComputerChoice();
+
+
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if(playerSelection === computerSelection){
+        return "This is a Stalemate!";  
+    } 
+        else if(playerSelection == 'rock'){
+            if(computerSelection == 'scissors'){
+                return ("You win! " + playerSelection + " beats " + computerSelection);
+            } else if(computerSelection == 'paper'){
+                return("You lose, " + computerSelection + " beats " + playerSelection);
+                }
+        }
+        else if(playerSelection == 'paper'){
+            if(computerSelection == 'rock'){
+                return ("You win! " + playerSelection + "  beats " + computerSelection);
+            } else if(computerSelection == 'scissors'){
+                return("You lose, " + computerSelection + " beats " + playerSelection);
+            }
+        }
+        else if(playerSelection == 'scissors'){
+            if(computerSelection == 'paper'){
+                return ("You win! " + playerSelection + " beats " + computerSelection);
+            } else if(computerSelection == 'rock'){
+                return("You lose, " + computerSelection + " beats " + playerSelection);
+
+            }
+        }
+    // console.log(computerSelection);
+    // console.log(playerSelection);
+}
+
+
+// console.log(playRound("Rock", getComputerChoice()));
+
+// console.log("Computer = " + getComputerChoice());
+
+console.log(playRound("Rock", computer))
+
+console.log("Computer = " + computer);
+    console.log("Player = Rock");
